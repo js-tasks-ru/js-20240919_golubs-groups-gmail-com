769b437f6b0823e83ea2e05a4b8d230b2e16bcd2
Alena Golub <golubs.groups@inbox.ru>
2024-09-30 20:56:24 +1000

    3.4 v2

--- 03-objects-arrays-intro-to-testing/4-uniq/index.js
+++ 03-objects-arrays-intro-to-testing/4-uniq/index.js
@@ -4,10 +4,6 @@
  * @returns {*[]} - the new array with uniq values
  */
 export function uniq(arr) {
-    const result = items.reduce((arr_new, arr) => {
-        if (arr_new.includes(arr)) {
-          return arr_new; 
-        }
-        return [...arr_new, arr]; 
-      }, []);
+    let uniqArr = new Set(arr);
+  return [...uniqArr];
 }
