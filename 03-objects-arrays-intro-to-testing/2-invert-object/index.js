56bec96471628fd81597af532fc0a8d62e6b755b
Alena Golub <golubs.groups@inbox.ru>
2024-10-03 01:10:53 +1000

    v7123

--- 03-objects-arrays-intro-to-testing/2-invert-object/index.js
+++ 03-objects-arrays-intro-to-testing/2-invert-object/index.js
@@ -4,10 +4,9 @@
  * @returns {object | undefined} - returns new object or undefined if nothing did't pass
  */
 export function invertObj(obj) {
-  const entries = Object.entries(obj);
-  const result = {};
- for (const [key, value] of entries) {
-   result[value] = pair[key];
- }
-  return result;
+  if (obj) {
+    return Object.fromEntries(
+      Object.entries(obj)
+      .map(([key, value]) => [key, value] = [value, key]));
+     }
 }
