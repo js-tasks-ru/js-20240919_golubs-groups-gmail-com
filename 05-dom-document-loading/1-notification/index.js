97894c0162e7e4801fc081269b753ee5c9f00ff6
Alena Golub <golubs.groups@inbox.ru>
2024-10-16 01:48:25 +1000

    5.1 5.2

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,46 @@
 export default class NotificationMessage {
 
-}
+    static activeNotification;
+    
+    constructor(message = "Success", params = {}){
+        this.message = message;
+        this.duration = params.duration || 2000;
+        this.type = params.type || "success";
+        this.element = this.createElement(this.createTemplate());
+    }
+    timerId;
+    createElement(template){
+        const element = document.createElement("div")
+        element.innerHTML = template;
+    return element.firstElementChild;
+    }
+    createTemplate(){
+        return`
+        <div class="notification ${this.type}" style="--value:${this.duration / 1000}s">
+        <div class="timer"></div>
+        <div class="inner-wrapper">
+          <div class="notification-header"> ${this.type}</div>
+          <div class="notification-body">
+           ${this.message}
+          </div>
+        </div>
+      </div>
+        `;
+    }
+    show(container = document.body){
+        if(NotificationMessage.activeNotification){
+            NotificationMessage.activeNotification.destroy();
+        }
+        NotificationMessage.activeNotification = this;
+        container.append(this.element);
+        this.timerId = setTimeout(() => this.destroy(), this.duration);
+    }
+    remove(){
+        this.element.remove();
+    }
+    destroy(){
+        clearTimeout(this.timerId);
+        this.remove();
+    }
+    }
+    
